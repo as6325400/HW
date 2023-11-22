e651478191b3fec295eee6cdc10f23bac8f5401e
as6325400 <as6325400@gmail.com>
2023-11-22 11:48:11 +0800

    HW1 finish

--- programming_112_1/HW1/410430049.c
+++ programming_112_1/HW1/410430049.c
@@ -2,8 +2,9 @@
 #include<stdlib.h>
 #include<stdbool.h>
 #include<string.h>
-#include <ncurses.h>
 #include <conio.h>
+
+char name[20];
 
 void clearTerminal(){
   printf("\x1B[2J\x1B[H");
@@ -22,7 +23,6 @@ void DisplayNoSuchFile(){
   clearTerminal();
   printf("No such level found\n");
   printf("Press any key to continue...");
-  _getch();
   _getch();
 }
 
@@ -59,8 +59,61 @@ char **readGraph(char name[], int *row, int *column){
     }
   }
 
+  fclose(fp);
   return map;
   
+}
+
+// up 0, left 1, right 2, down 3
+bool changeMap(char **map, int row, int column, int mode){
+  int direci[4] = {-1, 0, 0, 1};
+  int direcj[4] = {0, -1, 1, 0};
+
+  int pi, pj, newi, newj;
+  for(int i = 0; i < row; i++){
+    for(int j = 0; j < column; j++){
+      if(map[i][j] == 'P'){
+        pi = i;
+        pj = j;
+        break;
+      }
+    }
+  }
+
+  newi = pi + direci[mode];
+  newj = pj + direcj[mode];
+  if(newi >= row || newi < 0 || newj >= column || newj < 0) return false;
+  if(map[newi][newj] == ' ' || map[newi][newj] == 'o'){
+    if(map[newi][newj] == ' '){
+      map[newi][newj] = 'P';
+      map[pi][pj] = ' ';
+    }
+    else{
+      int newBoxi = newi + direci[mode], newBoxj = newj + direcj[mode];
+      if(map[newBoxi][newBoxj] == ' ' || map[newBoxi][newBoxj] == 'D'){
+        map[newi][newj] = 'P';
+        map[pi][pj] = ' ';
+        if(map[newBoxi][newBoxj] == 'D'){
+          map[newBoxi][newBoxj] = 'X';
+        }
+        else{
+          map[newBoxi][newBoxj] = 'o';
+        }
+      }
+      else return false;
+    }
+  }
+  else true;
+}
+
+bool newMap(char **map, int *row, int *column){
+  char **newmap = readGraph(name, row, column);
+  for(int i = 0; i < *row; i++){
+    for(int j = 0; j < *column; j++){
+      map[i][j] = newmap[i][j];
+    }
+  }
+  return true;
 }
 
 void displayMap(char **map, int row, int column){
@@ -75,14 +128,43 @@ void displayMap(char **map, int row, int column){
     }
     printf("\n");
   }
+  char direction = _getch();
 
-  sleep(3213213);
+  if(direction == 72) changeMap(map, row, column, 0);
+  if(direction == 80) changeMap(map, row, column, 3);
+  if(direction == 75) changeMap(map, row, column, 1);
+  if(direction == 77) changeMap(map, row, column, 2);
+  if(direction == 'R') newMap(map, &row, &column);
+}
+
+bool judgeGame(char **map, int row, int column){
+  for(int i = 0; i < row; i++){
+    for(int j = 0; j < column; j++){
+      if(map[i][j] == 'o') return false;
+    }
+  }
+  return true;
+}
+
+void winGame(){
+  printf("Congratulations on passing the level~\n");
+}
+
+bool replay(){
+  while(1){
+    printf("Do you want to continue playing?(Y/n)");
+    char ch = _getch();
+    if(ch == 'Y') return true;
+    else if(ch == 'n') return false;
+    printf("\n");
+  }
+  return false;
 }
 
 int main(){
   while(1){
     int row, column;
-    char **map, name[20];
+    char **map;
 
     DisplayMenu();
 
@@ -102,8 +184,12 @@ int main(){
       }
     }
 
-    displayMap(map, row, column);
+    while(!judgeGame(map, row, column)){
+      displayMap(map, row, column);
+    }
 
-
+    winGame();
+    if(replay()) continue;
+    else break;
   }
 }
