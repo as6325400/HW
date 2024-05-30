55463d214a4fa68def0c940a6507573db7379191
as6325400 <as6325400@gmail.com>
2024-05-30 15:27:11 +0800

    retrace backend

--- www/hw5/backend/src/models/sessionStore.ts
+++
@@ -1,9 +0,0 @@
-import session from 'express-session';
-import MySQLStore from 'express-mysql-session';
-import connection from './db';
-
-const MySQLStoreConstructor = MySQLStore(session as any);
-
-const sessionStore = new MySQLStoreConstructor({}, connection);
-
-export default sessionStore;
--- www/hw5/backend/src/routes/router.ts
+++
@@ -1,6 +0,0 @@
-import Route from "./route";
-import AuthRoute from "./auth.route";
-
-export const router: Array<Route> = [
-    new AuthRoute()
-]
