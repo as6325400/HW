a80ac5781a780ef704c3fb81ce6a3bff9b31714e
as6325400 <as6325400@gmail.com>
2024-05-30 15:27:43 +0800

    retrace

---
+++ www/hw5/backend/migrations/20240529070321-create-user.js
@@ -0,0 +1,47 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password_hash: {
+        type: Sequelize.STRING
+      },
+      google_id: {
+        type: Sequelize.STRING
+      },
+      login_method: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'login_methods',
+          key: 'id'
+        }
+      },
+      picture: {
+        type: Sequelize.STRING
+      },
+      is_email_verified: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
